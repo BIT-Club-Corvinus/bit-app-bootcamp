7e3f52b53f2d73d937b27a59b439e1ef923307fc
GyureszBa <gyorgy.dezsenyi@gmail.com>
2024-04-11 17:21:17 +0200

    adding auth context

--- MyFirstExpoApp/App.js
+++ MyFirstExpoApp/App.js
@@ -1,16 +1,35 @@
-import { useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import supabase from './lib/supabase-client';
 
+const SessionContext = createContext(null)
+
 export default function App() {
   const [text, setText] = useState("Plészholder")
+  const [session, setSession] = useState(null)  
+
+  useEffect(() => {
+    const subscription = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (event === 'SIGNED_OUT') {
+          setSession(null)
+        } else if (session) {
+          setSession(session)
+        }
+      })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [])
+
 
   async function insertData(text) {
     try {
       const { error } = await supabase
         .from('example')
-        .insert({name: text })
-      
+        .insert({ name: text })
+
       if (error) {
         throw new error
       }
@@ -20,13 +39,15 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={[{ color: '#fff' }]}>Sziasztok!</Text>
-      <TextInput style={styles.textInput} onChangeText={(value) => setText(value)}></TextInput>
-      <TouchableOpacity style={styles.button} onPress={() => insertData(text)}>
-        <Text>{text}</Text>
-      </TouchableOpacity>
-    </View>
+    <SessionContext.Provider value={session}>
+      <View style={styles.container}>
+        <Text style={[{ color: '#fff' }]}>Sziasztok!</Text>
+        <TextInput style={styles.textInput} onChangeText={(value) => setText(value)}></TextInput>
+        <TouchableOpacity style={styles.button} onPress={() => insertData(text)}>
+          <Text>{text}</Text>
+        </TouchableOpacity>
+      </View>
+    </SessionContext.Provider>
   );
 }
 
